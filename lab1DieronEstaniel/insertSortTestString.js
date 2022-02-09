35896f6ad829733645bbe947ecb24a17cf0c8a8d
xyxare <xyxar.estaniel-20@cpu.edu.ph>
2022-02-09 22:43:50 +0800

    Add test for random strings

--- lab1DieronEstaniel/insertSortTestString.js
+++ lab1DieronEstaniel/insertSortTestString.js
@@ -1,9 +1,7 @@
 import { faker } from '@faker-js/faker';
-import { insertSort } from 'insertionSort.js';
+import { insertSort } from './insertionSort.js';
 import * as fs from 'fs';
 
-
-const fs = require('fs');
 
 if (fs.existsSync("insertSortTimesString.txt")){
 
@@ -18,21 +16,22 @@ for (var j = 0; j < 100; j++){
     const time0 = Date.now();
 
     var testArray = [];
-    var fakerFunctions = [faker.random.word(), faker.name.findName(), faker.address.cityName()]
 
-    for (var i = 0; i < 10; i++){
+
+    for (var i = 0; i < 10000; i++){
+
+        var fakerFunctions = [faker.random.word(), faker.name.findName(), faker.address.cityName()]
 
         testArray.push(fakerFunctions[Math.floor(Math.random() * 3)]);
     }
 
-    finalArray = insertSort(testArray);
-    console.log(finalArray);
+    var finalArray = insertSort(testArray);
     
     const time1 = Date.now();
     const finalTime = time1 - time0;
     console.log(`Time to sort 10000 random words: ${finalTime} milliseconds`);
 
-    fs.appendFileSync("insertSortTimesString.txt", `Time to sort 10,000 numbers. Test ${j + 1}: ${finalTime} ms \n`, (err) => {
+    fs.appendFileSync("insertSortTimesString.txt", `Time to sort 10,000 words. Test ${j + 1}: ${finalTime} ms \n`, (err) => {
         if (err)
             console.log(err);
         else{
@@ -43,5 +42,5 @@ for (var j = 0; j < 100; j++){
     averageTime += finalTime;
 }
 
-console.log(`Average time to sort 10,000 numbers, done over 100 tests: ${averageTime / 100} ms`)
+console.log(`Average time to sort 10,000 words, done over 100 tests: ${averageTime / 100} ms`)
 
