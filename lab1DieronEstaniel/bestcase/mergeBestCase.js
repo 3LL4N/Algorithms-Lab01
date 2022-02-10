a1079d5f87f024f965f13331e1a9b2424d5851cd
unknown <dierongamer04@gmail.com>
2022-02-10 18:57:04 +0800

    Added best and worse case files

---
+++ lab1DieronEstaniel/bestcase/mergeBestCase.js
@@ -0,0 +1,51 @@
+import { mergeSort } from "../mergeSort.js"
+import * as fs from 'fs';
+
+if (fs.existsSync("mergeSortTimeNumbers.csv")){
+
+    fs.unlinkSync("mergeSortTimeNumbers.csv");
+
+}
+
+fs.appendFileSync("mergeSortTimeNumbers.csv","number_of_elements,average_sort_time \n")
+
+for (var k = 3; k <= 750; k++){
+    var averageTime = 0;
+
+
+    for (var j = 0; j < 2000; j++){
+
+       
+
+        var testArray = [];
+        
+        for (var i = 0; i < k; i++){
+
+            testArray.push(i);
+        }
+
+        const time0 = Date.now();
+
+        const finalArray = mergeSort(testArray);
+        
+        
+        const time1 = Date.now();
+        const finalTime = time1 - time0;
+
+
+
+        averageTime += finalTime;
+    }
+
+    console.log(`Average time to sort ${k} numbers, done over ${j} tests: ${averageTime / j} ms`);
+
+    fs.appendFileSync("mergeSortTimeNumbers.csv", `${k},${averageTime / j} \n`, (err) => {
+        if (err)
+            console.log(err);
+        else{
+            console.log('success');
+            } 
+    });
+};
+
+console.log("Test done!")
